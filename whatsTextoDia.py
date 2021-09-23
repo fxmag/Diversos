34949b28b126592ca47aff96a883de7efe834ff3
p.beroschi <beroschi.peter@gmail.com>
2021-09-23 07:31:25 -0300

    .

--- whatsTextoDia.py
+++ whatsTextoDia.py
@@ -20,18 +20,21 @@ driver.quit()
 
 
 # ABRIR NAVEGADOR
-py.click(152,746) 
+#py.click(152,746)           # PC Trabalho
+py.click(384, 1062)          # PC HOME
 sleep(3)
 
 # DIGITAR WEB.WHATSAPP 
-py.click(319,64) 
+#py.click(319,64)            # PC WORK
+py.click(201,51)            # PC HOME 
 sleep(3) 
 py.typewrite('https://web.whatsapp.com\n') 
 sleep(35)
 
 # COPIAR TEXTO E COLAR
 pc.copy(textocompl)
-py.click(80,274)
+#py.click(80,274)
+py.click(365,293)
 py.typewrite('arquivos\n',interval=0.3) 
 sleep(3) 
 py.typewrite('>>> *_TEXTO DO DIA_* <<<\n', interval=0.3)
@@ -43,6 +46,7 @@ sleep(1)
 py.typewrite('_Para continuar, clique abaixo e leia diretamente no site_ https://wol.jw.org/pt/wol/h/r5/lp-t\n', interval=0.3)
 
 # FECHAR NAVEGADOR
-py.click(1340,8)
+#py.click(1340,8)
+py.click(1896,10)
 sleep(2)
-#py.press('enter')
+py.press('enter')
